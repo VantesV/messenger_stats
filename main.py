17b36b5c3963443348557ec30ac5940545007f76
vantesv <vantesv@hotmail.com>
2024-02-04 21:16:32 -0500

    Initial skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+# Read the json file and convert to csv
+
+
+# Total message count per sender
+
+
+# Total word count per sender
+
+
+# Top N most frequent words per sender
+
+
+# Messages by time of day
+# Messages by time of week
+
